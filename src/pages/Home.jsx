c7e89884dbc82521f9f6d822c670be9052fbf169
tolipov <ziyovuddin.developer@gmail.com>
2024-02-10 16:37:14 +0500

    modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import logo14 from '../assets/logo14.png'
 import userimg from '../assets/tolipovblack.png'
+
+const Dialog = () => {
+    return (
+        <dialog id="my_modal_3" className="modal">
+            <div className="modal-box">
+                <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+                <h3 className="font-bold text-lg">Hello!</h3>
+                <p className="py-4">Press ESC key or click on ✕ button to close</p>
+            </div>
+        </dialog>
+    )
+}
 const Home = () => {
     const sty = {
         select: "bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
         label: "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    }
+    const addBlock = (id) => {
+        document.getElementById(id).showModal()
     }
     return (
         <main className='bg-gray-50 dark:bg-gray-900'>
@@ -14,7 +32,12 @@ const Home = () => {
                         <h3 className='text-xl font-semibold'>
                             Tolipov <br /> Ziyovuddin
                         </h3>
-                        <img src={userimg} alt="user" className='rounded-full h-12 mr-2 border-gray-900 border-2' />
+                        <div className="avatar online">
+                            <div className="w-16 mask rounded-full">
+                                <img src={userimg} />
+                            </div>
+                        </div>
+
                     </div>
                 </nav>
                 <div className='backdrop-blur-md bg-white/10 my-2 rounded-xl min-h-[400px] px-4 py-2'>
@@ -42,7 +65,6 @@ const Home = () => {
                     </form>
                     <div className='flex py-2'>
                         <h1 className='text-white text-4xl font-semibold hidden'>Hozircha hech narsa!</h1>
-
                         <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 min-w-[100%]">
                             <caption className="py-5 px-0 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-transparent">
                                 <h2><span className='text-primary-500'>CAMBRIDGE </span>|<span className='text-yellow-500'> 1A </span>|<span className='text-red-500'> IT </span></h2>
@@ -54,9 +76,10 @@ const Home = () => {
                                         ISM Familiya
                                     </th>
                                     <th scope="col" className="px-6 py-3">
-                                        <button>
+                                        <button onClick={() => addBlock('my_modal_3')}>
                                             <i className="fa-solid fa-plus text-xl" />
                                         </button>
+                                        <Dialog />
                                     </th>
                                     <th scope="col" className="px-6 py-3">
                                         Category
