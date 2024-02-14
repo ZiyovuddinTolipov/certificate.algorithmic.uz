038ca6e2abf7db1cf04d0ff0a3792e38c081d684
tolipov <ziyovuddin.developer@gmail.com>
2024-02-14 18:22:46 +0500

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,21 +14,21 @@ const Profile = () => {
                             <span className="label-text text-base font-semibold">Ismi</span>
                             <label className="input input-bordered flex items-center gap-2  input-primary w-full ">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                                <input type="text" className="grow bg-transparent" placeholder="Ziyovuddin" />
+                                <input type="text" className="grow bg-transparent" value="Ziyovuddin" disabled />
                             </label>
                         </div>
                         <div className='flex flex-col gap-2'>
                             <span className="label-text text-base font-semibold">Familiya</span>
                             <label className="input input-bordered flex items-center gap-2  input-primary w-full ">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                                <input type="text" className="grow bg-transparent" placeholder="Tolipov" />
+                                <input type="text" className="grow bg-transparent" value="Tolipov" disabled/>
                             </label>
                         </div>
                         <div className='flex flex-col gap-2'>
                             <span className="label-text text-base font-semibold">Foydalanuvchi ID</span>
                             <label className="input input-bordered flex items-center gap-2  input-primary w-full ">
                                 <kbd className="kbd kbd-sm">ID</kbd>
-                                <input type="text" className="grow bg-transparent" placeholder="123456789" />
+                                <input type="text" className="grow bg-transparent" value="123456789" disabled/>
                             </label>
                         </div>
                     </div>
