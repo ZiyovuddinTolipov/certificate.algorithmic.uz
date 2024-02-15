54e4a858cdb6599cce17329a770b2d662bfa594d
tolipov <ziyovuddin.developer@gmail.com>
2024-02-15 16:13:13 +0500

    mark

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,56 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';;
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import students from '../data/students.json';
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-function ResponsiveAppBar() {
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
+const OquvchilarJadvali = () => {
     return (
-        <>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>     
-        </>
+        <table className='text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 min-w-[100%] h-auto'>
+            <thead>
+                <tr>
+                    <th className='px-6 py-1 font-medium  whitespace-nowrap text-white'>Ism Familiya</th>
+                    {Object.keys(students.oquvchilar[0].baholar).map((sana, index) => (
+                        <th key={index}>{sana}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {students.oquvchilar.map((oquvchi, index) => (
+                    <tr className='border-b bg-gray-800 border-gray-700' key={index}>
+                        <td className='px-6 py-1'>{oquvchi.ism}</td>
+                        {Object.values(oquvchi.baholar).map((bahol, index) => (
+                            <td className='px-6 py-1' key={index}>{bahol}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
-}
-export default ResponsiveAppBar;
+};
+
+export default OquvchilarJadvali;
